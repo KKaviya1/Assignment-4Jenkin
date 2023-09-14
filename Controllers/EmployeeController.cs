45827b6c61a6b56110531dc2c1ecc92be0d92acf
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-09-14 15:17:18 +0530

    my commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using JenkinsAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+         
+        static List<Employee> Employees = new List<Employee>
+        {
+            new Employee(){EmpId=1, EmpName="Viya",Destination="Manager"},
+            new Employee(){EmpId=2, EmpName="Ravi",Destination="HR"},
+            new Employee(){EmpId=3, EmpName="Prakash",Destination="Developer"},
+         
+
+        };
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            return Ok(Employees);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest("Invalid Employee data.");
+            }
+
+            employee.EmpId = Employees.Max(e => e.EmpId) + 1; 
+            Employees.Add(employee);
+
+            return CreatedAtAction(nameof(Get), new { id = employee.EmpId }, employee);
+        }
+    }
+}
+
---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JenkinsAPI.Models
+{
+    public class Employee
+    {
+            
+        [Required]
+        public int EmpId { get; set; }
+        [Required]
+
+        public string EmpName { get; set; }
+        [Required]
+        public string Destination { get; set; }
+
+        internal static int Max(Func<object, object> value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
